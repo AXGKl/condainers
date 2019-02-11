94108db198cae3826e000830ec07cb7da41b1cd5
Gunther Klessinger <gk@axiros.com>
2019-02-11 03:11:46 +0100

    redir stderr

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./bootstrap "tests/spec1.yml" -N
+./bootstrap "tests/spec1.yml" -N 2>&1
 /opt/spec1_app/axc/enter tcpflow -h
 /opt/spec1_app/axc/enter hg -h
 /opt/spec1_app/axc/enter vim --version
