2b0d3d0906f7e3b81eae32fdd5da430bdefa3d10
Gunther Klessinger <gk@axiros.com>
2019-02-11 03:28:47 +0100

    Only omit token if local axc is set

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-./bootstrap "tests/spec1.yml" -N 2>&1
-/opt/spec1_app/axc/enter tcpflow -h
-/opt/spec1_app/axc/enter hg -h
-/opt/spec1_app/axc/enter vim --version
+sudo ./bootstrap "tests/spec1.yml" -N 2>&1
+sudo /opt/spec1_app/axc/enter tcpflow -h
+sudo /opt/spec1_app/axc/enter hg -h
+sudo /opt/spec1_app/axc/enter vim --version
 
 
