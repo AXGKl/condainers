536f611da018298c481702399e7db626364c2208
Gunther Klessinger <gk@axiros.com>
2019-02-11 03:37:19 +0100

    make sec avail in sudo

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+echo 'AXCHANGE_BASE_URL="'$AXCHANGE_BASE_URL'"' >> "tests/spec1.yml"
+echo 'axchange_token="'$axchange_token'"'       >> "tests/spec1.yml"
+
 sudo ./bootstrap "tests/spec1.yml" -N 2>&1
 sudo /opt/spec1_app/axc/enter tcpflow -h
 sudo /opt/spec1_app/axc/enter hg -h
